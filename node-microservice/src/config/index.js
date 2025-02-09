56f11373770ac28c04251425f718287b7b8c03b3
Kavindya Rajapaksha <it21825782@my.sliit.lk>
2025-02-10 01:15:11 +0530

    Dockerize the node application

--- node-microservice/src/config/index.js
+++ node-microservice/src/config/index.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 
 const config = {
-  PORT: process.env.PORT || 5001,
+  PORT: process.env.PORT || 5000,
   MONGO_URI: process.env.MONGO_URI,
   JWT_SECRET: process.env.JWT_SECRET,
   NODE_ENV: process.env.NODE_ENV || "development",

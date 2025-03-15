355555d505d0e5a6ef482306f37620da27edc100
Kavindya Rajapaksha <it21815782@my.sliit.lk>
2025-03-15 23:21:42 +0530

    Define typography

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,19 @@
 function App() {
   return (
     <>
-      <h1>Research Component</h1>
+      <h1>Heading 01</h1>
+      <h2>Heading 01</h2>
+      <h3>Heading 01</h3>
+
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate
+        iusto iure perspiciatis sed, ipsum officiis placeat quibusdam, totam
+        fugit autem aliquam rerum obcaecati aspernatur doloribus quos. Nostrum,
+        ad atque maiores tempora quo soluta ipsam eaque deserunt temporibus
+        voluptatem eos distinctio sequi praesentium fugiat eius dignissimos
+        tenetur illo natus, eveniet reprehenderit? Labore similique possimus
+        delectus exercitationem non aperiam perspiciatis officia.
+      </p>
     </>
   );
 }

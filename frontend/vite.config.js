5eade182520e193abf28c7c37b03fb0febf89b9f
Kavindya Rajapaksha <it21815782@my.sliit.lk>
2025-03-25 17:10:51 +0530

    Fix hot module reload in container instance

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,6 +5,11 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
+    watch: {
+      usePolling: true,
+    },
+    host: "0.0.0.0",
     port: 3000,
+    strictPort: true,
   },
 });

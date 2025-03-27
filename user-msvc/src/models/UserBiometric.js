d25afae9364e7531d163df9a64c62670e168d0ea
Kavindya Rajapaksha <it21815782@my.sliit.lk>
2025-03-27 10:27:10 +0530

    Update bio metrics mongoose schema

--- user-msvc/src/models/UserBiometric.js
+++ user-msvc/src/models/UserBiometric.js
@@ -7,8 +7,12 @@ const UserBiometricSchema = new mongoose.Schema(
     weight: { type: Number, required: true },
     bloodType: { type: String, required: true },
     salaryRange: { type: String, required: true },
+    basePremium: { type: Number, required: true },
+    habit: { type: Boolean, required: true },
+    maritalStatus: { type: String, required: true },
+    insurancePlan: { type: mongoose.Schema.Types.Mixed, default: null },
   },
-  { _id: false }
+  { _id: false } // ignore db generated '_id'
 );
 
 module.exports = mongoose.model("Biometric", UserBiometricSchema);
